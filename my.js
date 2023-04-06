72c9eadd84565c15d8534d73625e78765bd48dd2
Vinayakan <60319682+vinayakans@users.noreply.github.com>
2023-04-07 00:39:06 +0530

    Add files via upload

---
+++ my.js
@@ -0,0 +1,8 @@
+//Main heading hero
+var a = document.getElementById("imagineaplace");
+a.innerHTML = "imagine a place...";
+
+var b= document.getElementById("hero-contents");
+b.innerHTML = "...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.";
+ 
+
